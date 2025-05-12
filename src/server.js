9038e3289d9865b36d68884b0246f114299a6295
Ibnu Arseno <01123070008@kpndom.com>
2025-05-12 17:34:27 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -57,11 +57,14 @@ wss.on("connection", (ws) => {
 
 // API routes
 app.use("/api/images", imageRoutes);
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
 
 // Serve frontend
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // Mulai server
 server.listen(PORT, () => {
